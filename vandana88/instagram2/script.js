aebbe2dbfe0e141ef31b9ff99609ce673d71b85b
vandana88 <entcounsel96@gmail.com>
2014-07-23 20:49:44 -0400

    commit all

---
+++ vandana88/instagram2/script.js
@@ -0,0 +1,33 @@
+var options = {
+	type: 'GET',
+	dataType: 'jsonp',
+	url: 'https://api.instagram.com/v1/tags/darkto/media/recent?client_id=0d9085d9f9c249faa9abc68f8b2e8ed5',
+	success: successCallback,
+}
+
+function successCallback(response) {
+	var photosElement = document.getElementById('photos');
+	console.log(response);
+	response.data.forEach(function(photo) {
+		var html = '';
+		html += '<a class="col-md-3 photo" href="' + photo.link + '">';
+		html += '<img src="';
+		html += photo.images.standard_resolution.url;
+		html += '">';
+		html += '</a>';
+		photosElement.innerHTML += html;
+	});
+
+var options = {
+	type: 'GET',
+	dataType: 'jsonp',
+	url: response.pagination.next.url,  
+	success: successCallback,
+}
+$.ajax(options);
+
+
+}
+
+
+$.ajax(options);
